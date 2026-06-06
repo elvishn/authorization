877a77177713a360b8b8eb484df666f75bdde7b4
pavelbasmanov <pabasmanov@gmail.com>
2026-06-06 19:43:37 +0300

    NOISSUE: stabilize after commit

--- src/main/kotlin/com/elvishn/authorization/domain/value/Context.kt
+++
@@ -1,6 +0,0 @@
-package com.elvishn.authorization.domain.value
-
-data class Context(val ip: IPv4,
-    val userAgent: String,
-    val device: Device,
-    val phoneNumber: PhoneNumber?)
---
+++ src/main/kotlin/com/elvishn/identity/manager/api/AuthRequest.kt
@@ -0,0 +1,6 @@
+package com.elvishn.identity.manager.api
+
+data class AuthRequest(
+    val step: String,
+    val inputs: List<Any>
+)
